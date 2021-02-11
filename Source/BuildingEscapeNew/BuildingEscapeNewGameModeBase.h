9575e0e8cfffe9a959f740136704eb987959af27
Francis Sougué <francis.sougue1@gmail.com>
2021-02-11 10:21:25 +0200

    Backup before major refactor
    First Backup of Source files

---
+++ Source/BuildingEscapeNew/BuildingEscapeNewGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeNewGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPENEW_API ABuildingEscapeNewGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
